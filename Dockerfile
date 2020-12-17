8af7cc1c073027f6a3d57ee693365f5030e637d7
Tommy Giardina <tommy.giardina@gmail.com>
2020-12-17 14:34:57 -0600

    Adapted configs to work with rpp docker-compose

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:12
+WORKDIR /usr/src/app
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install
--- server/core/db.js
+++ server/core/db.js
@@ -30,11 +30,11 @@ module.exports = {
 
     let dbClient = null
     let dbConfig = (!_.isEmpty(process.env.DATABASE_URL)) ? process.env.DATABASE_URL : {
-      host: WIKI.config.db.host.toString(),
-      user: WIKI.config.db.user.toString(),
-      password: WIKI.config.db.pass.toString(),
-      database: WIKI.config.db.db.toString(),
-      port: WIKI.config.db.port
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      port: process.env.POSTGRES_PORT
     }
 
     // Handle SSL Options
